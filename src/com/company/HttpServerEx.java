9f07378c3af05816b214e2ae34a6384c3718eee1
SmelayaPanda <smelayapanda@mail.ru>
2016-06-03 00:36:44 +0600

    +

--- src/com/company/HttpServerEx.java
+++ src/com/company/HttpServerEx.java
@@ -2,22 +2,21 @@ package com.company;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.net.URL;
 import java.util.*;
 
 import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
+import com.sun.net.ssl.internal.ssl.Provider;
 
 public class HttpServerEx implements HttpHandler {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
-
         server.createContext("/", new HttpServerEx());
         server.start();
         System.out.println("Server started\nPress any key to stop...");
-
-
         System.in.read();
         server.stop(0);
         System.out.println("Server stoped");
@@ -27,26 +26,25 @@ public class HttpServerEx implements HttpHandler {
     public void handle(HttpExchange exc) throws IOException {
         exc.sendResponseHeaders(200, 0);
         PrintWriter out = new PrintWriter(exc.getResponseBody());
-        File myFolder = new File("C:/Program Files/");
-        File[] files = myFolder.listFiles();
-        for (int i = 0; i < files.length; i++) {
-            out.println("<html><body><a href=\"http://127.0.0.1:8080/\\" + files[i] + "\">" + files[i] + "</a></body></html><br>");
-            out.flush();
-        }
-        String a = exc.getRequestURI().getPath();
+        String a =  exc.getRequestURI().getPath();
+        File myFolder = new File(a);
 
-        if (a.equals("/Program Files/Notepad++")){
-
-            PrintWriter out2 = new PrintWriter(exc.getResponseBody());
-            File myFolder2 = new File("C:"+a+"/");
-            File[] files2 = myFolder2.listFiles();
-            for (int i = 0; i < files2.length; i++) {
-                out2.println("<html><body><a href=\"http://127.0.0.1:8080/\\" + files2[i] + "\">" + files2[i] + "</a></body></html><br>");
-                out2.flush();
+        out.write("<html><body>");
+        if (myFolder.isDirectory()) {
+            File[] directory = myFolder.listFiles();
+            for (int i = 0; i < directory.length; i++) {
+                out.write("<a href=\"http://127.0.0.1:8080/" + directory[i] + "\">" + directory[i] + "</a><br>");
+                out.write("</body></html>");
+                out.flush();
             }
+        } else if (myFolder.isFile()) {
+            File[] files = myFolder.listFiles();
+            for (int j = 0; j < files.length; j++)
+                out.write("<a href=\"http://127.0.0.1:8080/\\" + files[j] + "\"download>" + files[j] + "</a><br>");
+            out.flush();
+            out.write("</body></html>");
         }
-
         System.out.println(a);
-        out.flush();
+        out.close();
     }
 }
