9d507cf5493e311c1394910bc199fab55210db3d
SmelayaPanda <smelayapanda@mail.ru>
2016-06-03 00:56:02 +0600

    +

--- src/com/company/HttpServerEx.java
+++ src/com/company/HttpServerEx.java
@@ -26,25 +26,30 @@ public class HttpServerEx implements HttpHandler {
     public void handle(HttpExchange exc) throws IOException {
         exc.sendResponseHeaders(200, 0);
         PrintWriter out = new PrintWriter(exc.getResponseBody());
-        String a =  exc.getRequestURI().getPath();
+        String a = "C:" + exc.getRequestURI().getPath();
         File myFolder = new File(a);
 
         out.write("<html><body>");
+
         if (myFolder.isDirectory()) {
             File[] directory = myFolder.listFiles();
             for (int i = 0; i < directory.length; i++) {
-                out.write("<a href=\"http://127.0.0.1:8080/" + directory[i] + "\">" + directory[i] + "</a><br>");
-                out.write("</body></html>");
+                out.write("<a href=\"http://127.0.0.1:8080//" + directory[i] + "\">" + directory[i] + "</a><br>");
                 out.flush();
             }
+            out.write("</body></html>");
         } else if (myFolder.isFile()) {
             File[] files = myFolder.listFiles();
-            for (int j = 0; j < files.length; j++)
-                out.write("<a href=\"http://127.0.0.1:8080/\\" + files[j] + "\"download>" + files[j] + "</a><br>");
-            out.flush();
+            for (int j = 0; j < files.length; j++) {
+                if (a.equals(files[j].getAbsolutePath())) {
+                    out.write("<a href=\"http://127.0.0.1:8080//" + a + "\"download>" + a + "</a><br>");
+                }
+                out.write("<a href=\"http://127.0.0.1:8080//" + files[j] + "\"download>" + files[j] + "</a><br>");
+                out.flush();
+            }
             out.write("</body></html>");
         }
         System.out.println(a);
-        out.close();
+        // out.close();
     }
 }
