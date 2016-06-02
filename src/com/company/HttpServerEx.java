d40deff280d111aaf6fe3d44a4121b9f2b8c92e2
SmelayaPanda <smelayapanda@mail.ru>
2016-06-02 15:02:36 +0600

    +

--- src/com/company/HttpServerEx.java
+++ src/com/company/HttpServerEx.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.InetSocketAddress;
 import java.util.*;
 
+import com.sun.net.httpserver.Headers;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
@@ -11,9 +12,12 @@ import com.sun.net.httpserver.HttpServer;
 public class HttpServerEx implements HttpHandler {
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
         server.createContext("/", new HttpServerEx());
         server.start();
         System.out.println("Server started\nPress any key to stop...");
+
+
         System.in.read();
         server.stop(0);
         System.out.println("Server stoped");
@@ -21,24 +25,28 @@ public class HttpServerEx implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exc) throws IOException {
-
         exc.sendResponseHeaders(200, 0);
         PrintWriter out = new PrintWriter(exc.getResponseBody());
-        File myFolder = new File("C:\\Program Files\\Notepad++");
+        File myFolder = new File("C:/Program Files/");
         File[] files = myFolder.listFiles();
         for (int i = 0; i < files.length; i++) {
-            out.println("<html><body>" +
-                    "<a href=\"http://127.0.0.1:8080/\\" + files[i] + "\">" + files[i] + "</a>" +
-                    "</body></html>" + "<br>");
+            out.println("<html><body><a href=\"http://127.0.0.1:8080/\\" + files[i] + "\">" + files[i] + "</a></body></html><br>");
             out.flush();
         }
-        for (Map.Entry<String, List<String>> o : exc.getRequestHeaders().entrySet()) {
-            if (o.getKey().equals("Referer")) {
-                System.out.println(o.getValue());
+        String a = exc.getRequestURI().getPath();
+
+        if (a.equals("/Program Files/Notepad++")){
+
+            PrintWriter out2 = new PrintWriter(exc.getResponseBody());
+            File myFolder2 = new File("C:"+a+"/");
+            File[] files2 = myFolder2.listFiles();
+            for (int i = 0; i < files2.length; i++) {
+                out2.println("<html><body><a href=\"http://127.0.0.1:8080/\\" + files2[i] + "\">" + files2[i] + "</a></body></html><br>");
+                out2.flush();
             }
         }
-        out.close();
-        exc.close();
 
+        System.out.println(a);
+        out.flush();
     }
 }
