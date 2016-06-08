3e1ff026dc15b9231e332e4662e32b8a9208fb86
SmelayaPanda <smelayapanda@mail.ru>
2016-06-08 17:07:08 +0600

    +download file mechanism added

--- src/com/company/HttpServerEx.java
+++ src/com/company/HttpServerEx.java
@@ -10,6 +10,10 @@ import com.sun.net.httpserver.HttpServer;
 
 public class HttpServerEx implements HttpHandler {
     public static Stack<File> directoryStack = new Stack<File>();
+    public static File homePage = new File("C:\\");
+    String loopAddress = "\"http://127.0.0.1:8080//";
+    String localAddress = "\"http://192.168.0.102:8080//";
+    String downloadMessage = "<b>For download absolute file click me \\t&#x2714; </b><br>";
 
 
     public static void main(String[] args) throws IOException {
@@ -28,18 +32,18 @@ public class HttpServerEx implements HttpHandler {
         String a = "C:" + exc.getRequestURI().getPath();
         File myFolder = new File(a);
         out.write("<html><body>".getBytes());
-        out.write(("<a href=\"http://127.0.0.1:8080//" + directoryStack.peek() + "\">" + "<b>&#x21e6</b>" + "</a><br>").getBytes());
+        out.write(("<a href=" + loopAddress + homePage + "\">" + "<b>&#127968</b>" + "</a><br>").getBytes());
         if (myFolder.isDirectory()) {
             File[] directory = myFolder.listFiles();
             for (int i = 0; i < directory.length; i++) {
-                out.write(("<a href=\"http://127.0.0.1:8080//" + directory[i] + "\">" + directory[i] + "</a><br>").getBytes());
+                out.write(("<a href=" + loopAddress + directory[i] + "\">" + directory[i] + "</a><br>").getBytes());
             }
             out.write("</body></html>".getBytes());
             out.close();
             directoryStack.push(myFolder);
 
         } else if (myFolder.isFile()) {
-            out.write(("<a href=\"http://127.0.0.1:8080//" + a + "\"download>" + "<b>For download absolute file click me \t&#x2714; </b><br>" + "</a><br><br>").getBytes());
+            out.write(("<a href=" + loopAddress + a + "\"download>" + downloadMessage + "</a><br><br>").getBytes());
             out.write("</body></html>".getBytes());
             InputStream fileInput = new BufferedInputStream(new FileInputStream(myFolder));
             byte[] buf = new byte[4096];
