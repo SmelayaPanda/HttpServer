c91534ccf01017076871927606ae236d005cd7f9
SmelayaPanda <smelayapanda@mail.ru>
2016-06-08 17:38:46 +0600

    +

--- src/com/company/HttpServerEx.java
+++ src/com/company/HttpServerEx.java
@@ -2,6 +2,8 @@ package com.company;
 
 import java.io.*;
 import java.net.InetSocketAddress;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Stack;
 
 import com.sun.net.httpserver.HttpExchange;
@@ -9,16 +11,30 @@ import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
 public class HttpServerEx implements HttpHandler {
-    public static Stack<File> directoryStack = new Stack<File>();
     public static File homePage = new File("C:\\");
     String loopAddress = "\"http://127.0.0.1:8080//";
     String localAddress = "\"http://192.168.0.102:8080//";
-    String downloadMessage = "<b>For download absolute file click me \\t&#x2714; </b><br>";
+    String downloadMessage = "<b>For download absolute file click me &#x2714; </b><br>";
 
+    private static String getFileSizeKiloBytes(File file) {
+        return (double) file.length() / (1024) + " kb";
+    }
+
+    public long getFileSize(File folder) {
+        long foldersize = 0;
+        File[] filelist = folder.listFiles();
+        for (int i = 0; i < filelist.length; i++) {
+            if (filelist[i].isDirectory()) {
+                foldersize += getFileSize(filelist[i]);
+            } else {
+                foldersize += filelist[i].length();
+            }
+        }
+        return foldersize;
+    }
 
     public static void main(String[] args) throws IOException {
         File startFile = new File("C:\\");
-        directoryStack.push(startFile);
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", new HttpServerEx());
         server.start();
@@ -26,7 +42,6 @@ public class HttpServerEx implements HttpHandler {
 
     @Override
     public void handle(HttpExchange exc) throws IOException {
-
         exc.sendResponseHeaders(200, 0);
         OutputStream out = exc.getResponseBody();
         String a = "C:" + exc.getRequestURI().getPath();
@@ -40,10 +55,11 @@ public class HttpServerEx implements HttpHandler {
             }
             out.write("</body></html>".getBytes());
             out.close();
-            directoryStack.push(myFolder);
-
         } else if (myFolder.isFile()) {
-            out.write(("<a href=" + loopAddress + a + "\"download>" + downloadMessage + "</a><br><br>").getBytes());
+            Long lastModified = myFolder.lastModified();
+            SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            //System.out.println( sdf.format(new Date(lastModified)));
+            out.write(("<a href=" + loopAddress + a + "\"download>" + downloadMessage + "  " + getFileSizeKiloBytes(myFolder) + "<br>" + sdf.format(new Date(lastModified)) + "</a><br><br>").getBytes());
             out.write("</body></html>".getBytes());
             InputStream fileInput = new BufferedInputStream(new FileInputStream(myFolder));
             byte[] buf = new byte[4096];
@@ -53,7 +69,6 @@ public class HttpServerEx implements HttpHandler {
                 out.write(buf, 0, count);
             }
             out.close();
-            //directoryStack.push(myFolder);
         }
         System.out.println(a);
     }
