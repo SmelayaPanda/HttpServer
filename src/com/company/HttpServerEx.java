52d22c614f53f0e266c1305f3f1fdb552094db7e
SmelayaPanda <smelayapanda@mail.ru>
2016-06-08 13:39:41 +0600

    +download file mechanism added

--- src/com/company/HttpServerEx.java
+++ src/com/company/HttpServerEx.java
@@ -13,45 +13,35 @@ public class HttpServerEx implements HttpHandler {
         HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
         server.createContext("/", new HttpServerEx());
         server.start();
-        System.out.println("Server started\nPress any key to stop...");
-        System.in.read();
-        server.stop(0);
-        System.out.println("Server stoped");
     }
 
     @Override
     public void handle(HttpExchange exc) throws IOException {
         exc.sendResponseHeaders(200, 0);
-        //ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
         OutputStream out = exc.getResponseBody();
-        //PrintWriter out = new PrintWriter(exc.getResponseBody());
         String a = "C:" + exc.getRequestURI().getPath();
         File myFolder = new File(a);
         out.write("<html><body>".getBytes());
+
 
         if (myFolder.isDirectory()) {
             File[] directory = myFolder.listFiles();
             for (int i = 0; i < directory.length; i++) {
                 out.write(("<a href=\"http://127.0.0.1:8080//" + directory[i] + "\">" + directory[i] + "</a><br>").getBytes());
-                out.flush();
             }
             out.write("</body></html>".getBytes());
             out.close();
         } else if (myFolder.isFile()) {
-            out.write(("<a href=\"http://127.0.0.1:8080//" + a + "\"download>" + a + "</a><br>").getBytes());
-            out.flush();
+            out.write(("<a href=\"http://127.0.0.1:8080//" + a + "\"download>" + "<b>For download absolute file click me \t&#x2714; </b><br>"+ "</a><br><br>").getBytes());
             out.write("</body></html>".getBytes());
             InputStream fileInput = new BufferedInputStream(new FileInputStream(myFolder));
             byte[] buf = new byte[4096];
             int count;
-            DataOutputStream dos = new DataOutputStream(out);
+            out.write("<br><b>Text representation of file: </b><br>".getBytes());
             while ((count = fileInput.read(buf)) >= 0) {
-                dos.write(buf, 0, count);
+                out.write(buf, 0, count);
             }
-            out.flush();
             out.close();
-
-
         }
         System.out.println(a);
     }
